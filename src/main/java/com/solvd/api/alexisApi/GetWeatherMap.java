8dc6cf97aad77ed7879543f3da9326801b1c82e2
Alexisvi14 <alexisvillamayor96>
2022-06-24 10:45:14 -0300

    adding some api tests

--- src/main/java/com/solvd/api/alexisApi/GetWeatherMap.java
+++ src/main/java/com/solvd/api/alexisApi/GetWeatherMap.java
@@ -1,4 +1,4 @@
-package com.solvd.api;
+package com.solvd.api.alexisApi;
 
 import com.qaprosoft.carina.core.foundation.api.AbstractApiMethodV2;
 import com.qaprosoft.carina.core.foundation.api.annotation.Endpoint;
@@ -8,11 +8,13 @@ import com.qaprosoft.carina.core.foundation.api.http.HttpMethodType;
 import com.qaprosoft.carina.core.foundation.api.http.HttpResponseStatusType;
 import com.qaprosoft.carina.core.foundation.utils.Configuration;
 
-@Endpoint(url= "${base_url}/data/2.5/weather?lat=33.44&lon=-94.04&appid=ebcbce76c2e1c2539d1c46431a4455f6", methodType = HttpMethodType.GET)
-@ResponseTemplatePath(path = "api/weather/_get/rs.json")
-@SuccessfulHttpStatus(status = HttpResponseStatusType.OK_200)
-public class GetWeatherMethod extends AbstractApiMethodV2 {
+    @Endpoint(url= "${base_url}", methodType = HttpMethodType.GET)
+    @ResponseTemplatePath(path = "alexisApi/_get/city/rs.json")
+    @SuccessfulHttpStatus(status = HttpResponseStatusType.OK_200)
+    public class GetWeatherMap extends AbstractApiMethodV2 {
 
-    public GetWeatherMethod () {replaceUrlPlaceholder("base_url", Configuration.getEnvArg("api_url"));}
+        public GetWeatherMap() {
+            replaceUrlPlaceholder("base_url", Configuration.getEnvArg("api_url1"));
+        }
+    }
 
-}
